c45f799c4d7c5ec1f1233cd57b55590af6d2dace
Shawn <zS23014115@estudiantes.uv.mx>
2024-10-03 02:50:53 +0000

    perf: inline utility functions

--- src/Common.h
+++ src/Common.h
@@ -5,7 +5,7 @@
 #include <fstream>
 #include <functional>
 
-void each_line(const std::string &filename, const std::function<void(const std::string&)> &callback) {
+inline void each_line(const std::string &filename, const std::function<void(const std::string&)> &callback) {
   std::ifstream file(filename);
   std::string line;
   
@@ -14,21 +14,21 @@ void each_line(const std::string &filename, const std::function<void(const std::
   }
 }
 
-bool is_whitespace(const char &character) {
+inline bool is_whitespace(const char &character) {
   return character == ' ' or character == '\t' or character == '\n';
 }
 
-bool is_whitespace(const std::string &line) {
+inline bool is_whitespace(const std::string &line) {
   return std::all_of(line.begin(), line.end(), [](const char &character) {
     return is_whitespace(character);
   });
 }
 
-void println(const std::string &line) {
+inline void println(const std::string &line) {
   std::cout << line << std::endl;
 }
 
-std::string trim(const std::string &line) {
+inline std::string trim(const std::string &line) {
   std::string trimmed = line;
   trimmed.erase(0, trimmed.find_first_not_of(" \t\n"));
   trimmed.erase(trimmed.find_last_not_of(" \t\n") + 1);
