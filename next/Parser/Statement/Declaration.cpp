f82ca564026530699e4a3917fca9264c481f9d08
OGShawnLee <OGShawnLee@gmail.com>
2026-06-06 17:50:45 -0600

    chore: remove legacy C++ ports and shell installer script

--- next/Parser/Statement/Declaration.cpp
+++
@@ -1,9 +0,0 @@
-#include "Declaration.h"
-
-Declaration::Declaration(STATEMENT_TYPE statement_type, const std::string &identifier)  : Statement(statement_type) {
-  this->identifier = identifier;
-}
-
-std::string Declaration::get_identifier() const {
-  return identifier;
-}
